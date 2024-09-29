c8d1ef9b6105636950abbfaf50da0f409bc90c5f
Rohit Sarpotdar <rohi7ns@gmail.com>
2024-09-30 00:08:23 +0530

    updated css with tailwind classes added loadash, router

--- src/main.js
+++ src/main.js
@@ -5,27 +5,27 @@ import { initFlowbite } from 'flowbite'
 import $ from 'jquery'
 
   // initialize components based on data attribute selectors
-$(document).ready(function() {
+$(function() {
   initFlowbite()
 });
 
 $('#app').html(`
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    <a href="https://vitejs.dev" target="_blank" class="font-medium text-[#646cff] [text-decoration:inherit] hover:text-[#535bf2]">
+      <img src="${viteLogo}" class="h-[6em] p-[1.5em] [transition:filter_300ms] hover:[filter:drop-shadow(0_0_2em_#646cffaa)]" alt="Vite logo" />
     </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" class="font-medium text-[#646cff] [text-decoration:inherit] hover:text-[#535bf2]">
+      <img src="${javascriptLogo}" class="h-[6em] p-[1.5em] [transition:filter_300ms] hover:[filter:drop-shadow(0_0_2em_#646cffaa)] hover:[filter:drop-shadow(0_0_2em_#f7df1eaa)]" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
+    <h1 class="text-slate-900 dark:text-white text-[3.2em] leading-[1.1]">Hello Vite!</h1>
+    <div class="p-[2em]">
       <button 
         id="counter"
         type="button"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+        class="rounded-[8px] border-[1px] border-solid border-[transparent] px-[1.2em] py-[0.6em] text-[1em] font-medium [font-family:inherit] bg-[#1a1a1a] cursor-pointer [transition:border-color_0.25s] hover:border-[#646cff] outline-[4px_auto_-webkit-focus-ring-color]">
       </button>
     </div>
-    <p class="read-the-docs">
+    <p class="text-slate-900 dark:text-white">
       Click on the Vite logo to learn more
     </p>
   </div>
